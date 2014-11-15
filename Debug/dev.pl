2d1f14c01875afd91d8d815c70de93dee61507b2
s.sikder <s.sikder@samsung.com>
2014-11-15 17:39:57 +0530

    Script Changed

--- Debug/dev.pl
+++ Debug/dev.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 my @devices = split('\n\n', `usb-devices`);
 my @samdevs = ();
-while(($_ = shift @devices)&& $_ =~/manufacturer=samsung/i){
-    push (@samdevs, $1) if m/serialnumber=([^\n]+)\n/i;
+while($_ = shift @devices) {
+	if ($_ =~ m/manufacturer\=samsung/i){
+    		push (@samdevs, $1) if ($_ =~ m/serialnumber\=([^\n]+)\n/i);
+	}
 }
-
+print "@samdevs\n";
 open(OUT, ">", "final.info");
 print OUT "@samdevs\n";
